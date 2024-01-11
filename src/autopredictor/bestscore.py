be7edf7eba6395c6b72fe2d59f3eefdcbb953b17
Anu Banga <anubanga@gmail.com>
2024-01-11 13:30:17 -0800

    Added bestscore.py to display best score

---
+++ src/autopredictor/bestscore.py
@@ -0,0 +1,16 @@
+def display_best_score(X):
+    """
+    This function Identifies the best score with respect to a specific scoring metric along with the corresponding model.
+    It gives the DataFrame output in a table format.
+
+    Parameters
+    ----------
+    X : DataFrame
+        A DataFrame containing all scoring metrics results alongside the corresponding model, sorted alphabetically.
+
+    Returns
+    -------
+    DataFrame
+        A DataFrame containing the best score with respect to a specific scoring metric alongside the corresponding 
+        model.
+    """
