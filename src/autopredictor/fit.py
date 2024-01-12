0d5f8f967f8f7f04bc1928bc923a64cda399e5af
Arturo Rey <63738521+AReyH@users.noreply.github.com>
2024-01-11 21:09:27 -0800

    Update fit.py
    Changed the format of the docstring

--- src/autopredictor/fit.py
+++ src/autopredictor/fit.py
@@ -3,15 +3,23 @@ def fit(X_train,X_test,y_train,y_test):
     """
     Train and evaluate multiple regression models on the given training and test data.
 
-    Parameters:
-    - X_train (Pandas DataFrame): Training data features.
-    - X_test (Pandas DataFrame): Test data features.
-    - y_train (Pandas Series): Training data target values.
-    - y_test (Pandas Series): Test data target values.
+    Parameters
+    ----------
+    X_train: Pandas DataFrame 
+             Training data features.
+    X_test:  Pandas DataFrame 
+             Test data features.
+    y_train: Pandas Series
+             Training data target values.
+    y_test:  Pandas Series 
+             Test data target values.
 
-    Raises:
-    - ValueError: If any of the inputs are empty or None.
+    Raises
+    ------
+    ValueError: If any of the inputs are empty or None.
 
-    Returns:
-    - dict: A dictionary containing the performance scores for each model and metric.
+    Returns
+    -------
+    dict 
+        A dictionary containing the performance scores for each model and metric.
     """
