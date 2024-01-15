8c715b6e5ebf9e253654a80f122e7d51b3fcd799
Arturo Rey Haggar <arturo.rey.h@outlook.com>
2024-01-15 14:55:01 -0800

    Added fit.py reluctantly again

--- src/autopredictor/fit.py
+++ src/autopredictor/fit.py
@@ -1,12 +1,12 @@
-
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.svm import LinearSVR, SVR
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
-from sklearn.metrics import mean_absolute_error, r2_score, mean_absolute_percentage_error
+from sklearn.metrics import mean_absolute_error, r2_score, mean_absolute_percentage_error, mean_squared_error
 from sklearn.datasets import load_diabetes
-
-
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
 
 
 def fit(X_train,X_test,y_train,y_test):
@@ -33,9 +33,7 @@ def fit(X_train,X_test,y_train,y_test):
     dict 
         A dictionary containing the performance scores for each model and metric.
     """
-
-def fit(X_train,X_test,y_train,y_test):
-    if X_train and X_test and y_train and y_test:
+    if X_train is not None and X_test is not None and y_train is not None and y_test is not None:
         scores_list = {}
 
         models = {
@@ -50,11 +48,7 @@ def fit(X_train,X_test,y_train,y_test):
             'AdaBoost': AdaBoostRegressor()
         }
 
-        metrics = {
-            'Mean Absolute Error': mean_absolute_error(),
-            'Mean Absolute Percentage Error': mean_absolute_percentage_error(),
-            'R2 Score': r2_score()
-        }
+        
 
         for name, model in models.items():
             model.fit(X_train,y_train)
@@ -62,17 +56,26 @@ def fit(X_train,X_test,y_train,y_test):
 
             scores_list[name] = {}
 
+            metrics = {
+                'Mean Absolute Error': mean_absolute_error(y_test, model.predict(X_test)),
+                'Mean Absolute Percentage Error': mean_absolute_percentage_error(y_test, model.predict(X_test)),
+                'R2 Score': r2_score(y_test, model.predict(X_test)),
+                'Mean Squared Error': mean_squared_error(y_test, model.predict(X_test)),
+                'Root Mean Squared Error': np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
+                
+
+            }
+
             for metric_name, metric in metrics.items():
 
-                scores_list[name][metric_name] = metric(y_test, model.predict(X_test))
+                scores_list[name][metric_name] = metric
 
     else:
-        raise('Please input a valid DataFrame')
-    
+        raise Exception('Please input a valid DataFrame')
 
 
+    return scores_list
 
 
-
-
-
+if __name__ == '__main__':
+    fit()
