b0c7eb5899c12283db03c40a91ecdd4af07b3a97
Anu Banga <anubanga@gmail.com>
2024-01-18 05:40:14 -0800

    Added error handling in function

--- src/autopredictor/bestscore.py
+++ src/autopredictor/bestscore.py
@@ -16,19 +16,44 @@ def display_best_score(X, scoring_metric):
     Returns
     -------
     DataFrame
-        A DataFrame containing the best score with respect to a specific scoring metric alongside the corresponding 
-        model.
-    """
+        If the scoring metric is found, a dataframe containing the best score and the corresponding model is returned.
+        If the scoring metric is not found, a ValueError is raised.
+
+    Examples
+    --------
+    >>> from autopredictor.bestscore import display_best_score
+    >>> df = pd.DataFrame({'MAE': [5.6, 3.4],
+                                  'MSE': [9.4, 21.4],
+                                  'MAPE': [0.34, 0.45]},
+                                 index=['Linear Regression', 'Random Forest'])
+    >>> display_best_score(df, 'MAE')
+                       MAE  
+    Linear Regression  5.6
+    
+    >>> display_best_score(df, 'R2')
+    ValueError: Invalid Scoring metric 'R2'.The specified metric is not in the list of available metrics. Available metrics: MAE, MSE, MAPE.
+   """
+    if X is None or not isinstance(X, pd.DataFrame):
+        raise TypeError("Invalid DataFrame provided.")
+    
+    if X.empty:
+        raise TypeError("DataFrame is empty.")
+
     if scoring_metric not in X.columns:
-        print(f"'{scoring_metric}' not found in the DataFrame.")
-        return None
+        available_metrics = X.columns.tolist()
+        available_metrics_string = ", ".join(available_metrics)
+        raise ValueError (f"Invalid Scoring metric '{scoring_metric}'.The specified metric is not in the list of available metrics. Available metrics: {available_metrics_string}.")
+    
+    if X[scoring_metric].isnull().any():
+        raise ValueError(f"Invalid Scoring metric '{scoring_metric}'. The specified metric contains null values. Please handle or remove null values before using this function.")
 
-    max_score = X.loc[X[scoring_metric].idxmax()]
+    best_model = X[scoring_metric].idxmax()
+    best_score = X.loc[best_model, scoring_metric]
 
-    result_table = pd.DataFrame([max_score])
-    print(tabulate(result_table, headers='keys', tablefmt='github', showindex=False))
+    result_table = pd.DataFrame({scoring_metric: [best_score]}, index=[best_model])
+    print(tabulate(result_table, headers='keys', tablefmt='github', showindex=True))
 
-    return max_score
+    return best_score, best_model
 
 if __name__ == '__main__':
     display_best_score()
