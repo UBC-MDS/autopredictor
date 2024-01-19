f295ba622b1be9954dd282e9584d6ea10ddda9b0
Arturo Rey Haggar <arturo.rey.h@outlook.com>
2024-01-18 16:59:37 -0800

    Test

--- src/autopredictor/fit.py
+++ src/autopredictor/fit.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 
 
-def fit(X_train,X_test,y_train,y_test):
+def fit(X_train,X_test,y_train,y_test,return_train=False):
     """
     Train and evaluate multiple regression models on the given training and test data.
 
@@ -32,7 +32,8 @@ def fit(X_train,X_test,y_train,y_test):
         A dictionary containing the performance scores for each model and metric.
     """
     if X_train is not None and X_test is not None and y_train is not None and y_test is not None:
-        scores_list = {}
+        scores_list_train = {}
+        scores_list_test = {}
 
         models = {
             'Linear Regression': LinearRegression(),
@@ -52,28 +53,46 @@ def fit(X_train,X_test,y_train,y_test):
             model.fit(X_train,y_train)
             print(f'{name} trained.')
 
-            scores_list[name] = {}
+            scores_list_train[name] = {}
 
-            metrics = {
+            # Train scores
+
+            if return_train:
+                metrics_train = {
+                    'Mean Absolute Error': mean_absolute_error(y_train, model.predict(X_train)),
+                    'Mean Absolute Percentage Error': mean_absolute_percentage_error(y_train, model.predict(X_train)),
+                    'R2 Score': r2_score(y_train, model.predict(X_train)),
+                    'Mean Squared Error': mean_squared_error(y_train, model.predict(X_train)),
+                    'Root Mean Squared Error': np.sqrt(mean_squared_error(y_train, model.predict(X_train)))
+                }
+
+
+                for metric_name, metric in metrics_train.items():
+
+                    scores_list_train[name][metric_name] = metric
+
+            else:
+                pass
+
+            scores_list_test[name] = {}
+
+            metrics_test = {
                 'Mean Absolute Error': mean_absolute_error(y_test, model.predict(X_test)),
                 'Mean Absolute Percentage Error': mean_absolute_percentage_error(y_test, model.predict(X_test)),
                 'R2 Score': r2_score(y_test, model.predict(X_test)),
                 'Mean Squared Error': mean_squared_error(y_test, model.predict(X_test)),
                 'Root Mean Squared Error': np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
-                
-
             }
 
-            for metric_name, metric in metrics.items():
 
-                scores_list[name][metric_name] = metric
+            for metric_name, metric in metrics_test.items():
+
+                scores_list_test[name][metric_name] = metric
 
     else:
         raise Exception('Please input a valid DataFrame')
 
-
-    return scores_list
-
-
-if __name__ == '__main__':
-    fit()
+    if return_train:
+        return (scores_list_train, scores_list_test)
+    else:
+        return (scores_list_test)
