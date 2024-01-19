5624c4c02b3a2e099f9535b3a843dca27486f478
Sharon Voon <svoon12@gmail.com>
2024-01-18 22:26:10 -0800

    Added Raises in docstring

--- src/autopredictor/show_all.py
+++ src/autopredictor/show_all.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from tabulate import tabulate
 
-def show_all(X):
+def show_all(result):
     """
     This function converts the trained regression model scores stored in a dictionary by 
     the "fit" function into a DataFrame, sorted alphabetically by model. It also 
@@ -9,7 +9,7 @@ def show_all(X):
 
     Parameters
     ----------
-    X : dict
+    result : dict
         A dictionary containing scoring metrics data for each regression model. The keys 
         represent model names where the values represent the scoring results and should 
         be numeric.
@@ -19,6 +19,16 @@ def show_all(X):
     DataFrame
         A DataFrame containing all scoring metrics results alongside the corresponding 
         model, sorted alphabetically.
+
+
+    Raises
+    ------
+    TypeError
+        If result is not a dictionary.
+    ValueError
+        If result dictionary is empty.
+        If there is invalid scoring metrics in the result dictionary's value.
+        If the number of scoring metrics are not correct.
 
     Examples
     --------
@@ -38,17 +48,17 @@ def show_all(X):
     >>> test_scores = show_all(model_scores)        
     """
 
-    # Check if X is of type dictionary
-    if not isinstance(X, dict):
+    # Check if result is of type dictionary
+    if not isinstance(result, dict):
         raise TypeError("Input should be of dictionary type.")
     
-    # Check if X is an empty dictionary
-    if X == {}:
+    # Check if result is an empty dictionary
+    if result == {}:
         raise ValueError("Input should not be an empty dictionary. No training scores are avaialble. Call fit function to test the model.")
     
     # Check if inner dictionaries have correct scoring metrics
     valid_metrics = {"MEAN ABSOLUTE ERROR", "MEAN ABSOLUTE PERCENTAGE ERROR", "R2 SCORE", "MEAN SQUARED ERROR", "ROOT MEAN SQUARED ERROR"}
-    for model_name, model_score in X.items():
+    for model_name, model_score in result.items():
         if not all(metric.upper() in valid_metrics for metric in model_score):
             raise ValueError(f"Invalid scoring metrics for model.")
         if len(model_score) != 5:
@@ -63,13 +73,13 @@ def show_all(X):
         'ROOT MEAN SQUARED ERROR': 'RMSE'
     }
 
-    for model_name, model_metric in X.items():
+    for model_name, model_metric in result.items():
         updated_scores = {key_mapping[key.upper()]:value for key, value in model_metric.items()}
         X[model_name] = updated_scores
     
         
     # Convert the dictionary to a DataFrame
-    result_df = pd.DataFrame.from_dict(X, orient='index')
+    result_df = pd.DataFrame.from_dict(result, orient='index')
     result_df = result_df.sort_index()
 
     # Convert DataFrame to tabulate and print it in table format
